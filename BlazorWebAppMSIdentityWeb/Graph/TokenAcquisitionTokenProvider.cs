8afc13763c25a3e41a252b74e09284d4471d21d0
Luke Latham <1622880+guardrex@users.noreply.github.com>
2024-09-05 11:55:01 -0400

    Updates (#1)

---
+++ BlazorWebAppMSIdentityWeb/Graph/TokenAcquisitionTokenProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Claims;
+using Microsoft.Identity.Web;
+using Microsoft.Kiota.Abstractions.Authentication;
+
+namespace BlazorWebAppMSIdentityWeb.Graph;
+
+public class TokenAcquisitionTokenProvider(
+    ITokenAcquisition tokenAcquisition,
+    string[] scopes,
+    ClaimsPrincipal? user) : IAccessTokenProvider
+{
+    private readonly string[] validHosts =
+    [
+        "graph.microsoft.com",
+        "graph.microsoft.us",
+        "dod-graph.microsoft.us",
+        "graph.microsoft.de",
+        "microsoftgraph.chinacloudapi.cn",
+    ];
+
+    private readonly ClaimsPrincipal user = user ?? throw new Exception("User claims principal is required.");
+
+    public AllowedHostsValidator AllowedHostsValidator => new(validHosts);
+
+    public async Task<string> GetAuthorizationTokenAsync(
+        Uri uri,
+        Dictionary<string, object>? additionalAuthenticationContext = null,
+        CancellationToken cancellationToken = default)
+    {
+        if (!AllowedHostsValidator.IsUrlHostValid(uri))
+        {
+            return string.Empty;
+        }
+
+        if (uri.Scheme != "https")
+        {
+            throw new Exception("URL must use https.");
+        }
+
+        return await tokenAcquisition.GetAccessTokenForUserAsync(scopes, user: user);
+    }
+}
